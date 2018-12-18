cec11ff909bf525c261d41a3e692680ad191c93a
Matt Zuzolo <mattzuzolo@outlook.com>
2018-12-18 15:22:58 -0500

    first children classes built

--- index.js
+++ index.js
@@ -20,6 +20,45 @@ class Vehicle {
   };
 }
 
+class Car extends Vehicle {
+  constructor(make, model, topSpeed, color, year, numberOfWheels, fuelType, dealership){
+    super(make, model, topSpeed, color, year);
+    this.numberOfWheels = numberOfWheels;
+    this.fuelType = fuelType;
+    this.dealership = dealership;
+  }
+
+  fillGasTank(){
+    console.log(`Filling ${this.model} with ${this.fuelType} gas`);
+  }
+
+  payToll(){
+    console.log(`${this.make} ${this.model} is paying the toll`);
+  }
+}
+
+class Airplane extends Vehicle {
+  constructor(make, model, topSpeed, color, year, numberOfWings, crewSize, foodOptions){
+    super(make, model, topSpeed, color, year);
+    this.numberOfWheels = numberOfWings;
+    this.crewSize = crewSize;
+    this.foodOptions = foodOptions;
+  }
+
+  serveFood(){
+    console.log(`${this.make} ${this.model} has these options to eat:`);
+    this.foodOptions.forEach(option => console.log(option))
+  }
+
+  takeOff(){
+    console.log(`${this.model} is about to takeoff`);
+  }
+
+  land(){
+    console.log(`${this.model} is about to land`);
+  }
+}
+
 let vehicleOne = new Vehicle("Jeep", "Wrangler", 75, "black", 2016);
 let vehicleTwo = new Vehicle("Boeing", "787", 600, "white", 2018);
 
@@ -28,3 +67,28 @@ console.log("vehicleTwo:", vehicleTwo);
 
 vehicleOne.selfIdentifier();
 vehicleTwo.accelerate();
+
+console.log("—————————");
+
+let carOne = new Car("Honda", "Civic", 85, "red", 2019, 4, "regular", "Dealship A");
+let carTwo = new Car("Audi", "R8", 160, "silver", 2017, 4, "premium", "Dealship C");
+
+console.log("carOne", carOne);
+console.log("carTwo", carTwo);
+
+carOne.fillGasTank();
+carTwo.fillGasTank();
+carTwo.payToll();
+
+console.log("—————————");
+
+let airplaneOne = new Airplane("Boeing", "737", 500, "blue", 2004, 2, 5, ["Pretzels", "Peanuts"]);
+let airplaneTwo = new Airplane("Airbus", "A380", 550, "Gold", 2017, 2, 12, ["Chicken", "Pasta", "Fish"]);
+
+console.log("airplaneOne", airplaneOne);
+console.log("airplaneTwo", airplaneTwo);
+
+airplaneOne.serveFood();
+airplaneTwo.serveFood();
+airplaneOne.takeOff();
+airplaneTwo.land();
